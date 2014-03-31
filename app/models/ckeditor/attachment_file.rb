3d0eb6c9855fa736c1e3d8d25d2178b3ec645941
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-03-31 16:46:59 +0800

    fix nav url

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,7 +1,9 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
+    				:storage => :s3,
+  					:s3_credentials => S3_CREDENTIALS,
                     :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+                    :path => "/ckeditor_assets/attachments/:id/:filename"
 
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,7 +1,9 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
+      				:storage => :s3,
+  					:s3_credentials => S3_CREDENTIALS,
                     :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                    :path => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :styles => { :content => '800>', :thumb => '118x100#' }
 
   validates_attachment_size :data, :less_than => 2.megabytes
