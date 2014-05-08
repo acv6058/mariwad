96b220beadf20fb5b8e01a257e8eb748a5eea468
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-05-08 16:20:11 +0800

    enable banners to other pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,22 @@ class PagesController < ApplicationController
   layout 'pages'
 
   def show
+    @banners = []
     @page = Page.active.where('page_url = ?', params[:page_url]).first
+    unless @page.nil?
+    @banners = @page.banners
+    end
 
 
     #render :json => @page.has_parent?
   end
 
   def contacts
+    @banners = []
     @page = Page.contact
     @contact = Contact.first
+    unless @page.nil?
+      @banners = @page.banners
+    end
   end
 end
