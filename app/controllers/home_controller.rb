45478fea17637235500702c84a3db5bc36acc05e
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-10-23 16:48:34 +0800

    fix url for og:image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     @banners = Banner.where('featured = ? AND activate = ?', true, true)
     @page = Page.where(is_root: true).first
-    @meta = { title: 'Mariveles Water District', description: @page.meta_description ? @page.meta_description : '', url: Rails.application.routes.default_url_options[:host], image:  asset_url('mariwad_office.jpg') }
+    @meta = { title: 'Mariveles Water District', description: @page.meta_description ? @page.meta_description : '', url: Rails.application.routes.default_url_options[:host], image:  asset_url('assets/mariwad_office.jpg') }
     #render json: @meta#_description
   end
 end
