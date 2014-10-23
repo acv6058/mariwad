1bc2203d82bbac09014a5bd4fca8de870ce877bf
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-10-23 16:06:56 +0800

    Addthis plugin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   layout 'home'
-
+  include ActionView::Helpers::AssetUrlHelper
   def index
     @banners = Banner.where('featured = ? AND activate = ?', true, true)
     @page = Page.where(is_root: true).first
-    @meta = { title: 'Mariveles Water District', description: @page.meta_description ? @page.meta_description : '', url: Rails.application.routes.default_url_options[:host] }
+    @meta = { title: 'Mariveles Water District', description: @page.meta_description ? @page.meta_description : '', url: Rails.application.routes.default_url_options[:host], image:  asset_url('mariwad_office.jpg') }
+    #render json: @meta#_description
   end
 end
